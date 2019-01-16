e044cf5f40824519f3c0d46c89e8f9bb910c6923
Matěj Humpál <finwe@finwe.info>
2019-01-16 09:36:26 +0100

    Rename and use PNG compression level

--- src/Output/Png.php
+++ src/Output/Png.php
@@ -12,11 +12,11 @@ class Png
 	 * @param int QR code width in pixels
 	 * @param int[] $background RGB background color
 	 * @param int[] $color RGB foreground and border color
-	 * @param int Compression level (0 - no compression, 9 - greatest compression)
+	 * @param int $compression Level (0 - no compression, 9 - greatest compression)
 	 *
 	 * @return string Binary image data
 	 */
-	public function output(QrCode $qrCode, $w = 100, $background = [255, 255, 255], $color = [0, 0, 0], $quality = 0)
+	public function output(QrCode $qrCode, $w = 100, $background = [255, 255, 255], $color = [0, 0, 0], $compression = 0)
 	{
 		$qrSize = $qrCode->getQrSize();
 		$final = $qrCode->getFinal();
@@ -53,7 +53,7 @@ class Png
 		}
 
 		ob_start();
-		imagepng($im);
+		imagepng($im, null, $compression);
 		$data = ob_get_contents();
 		ob_end_clean();
 
